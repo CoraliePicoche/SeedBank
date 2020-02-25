51a935f10b178afa5ba38d875b036f25e7dcfd03
Coralie Picoche <coralie.picoche@u-bordeaux.fr>
2020-02-25 15:03:58 +0100

    Compared different ways of computing growth rates

--- script/comparison_growth_rate.r
+++ script/comparison_growth_rate.r
@@ -26,13 +26,36 @@ T_max=max_temp+273
 T_opt=temp #We are in the best possible conditions
 
 B=optimize(f_to_optimize,T_min,T_max,T_opt,A,interval=c(0,100))$minimum
+print(B)
 
 gr_min_SV=growth_rate_SV(T_min,T_opt,B)
 gr_max_SV=growth_rate_SV(T_max,T_opt,B)
 print(paste("Scranton and Vasseur",gr_min_SV,gr_max_SV))
 
+seq_temp=seq(T_min,T_max,length.out=1000)
+val_growth_SV=c()
+val_growth_Biss=c()
+
+for(t in seq_temp){
+	val_growth_SV=c(val_growth_SV,growth_rate_SV(t,T_opt,B))
+	val_growth_Biss=c(val_growth_Biss,growth_rate_Bissinger(t-273,0.5))
+}
 ##Bissinger
 
 gr_min_Bissinger=growth_rate_Bissinger(min_temp,0.5)
 gr_max_Bissinger=growth_rate_Bissinger(max_temp,0.5)
 print(paste("Bissinger",gr_min_Bissinger,gr_max_Bissinger))
+
+#Val in the literature
+gr_min_lit=0.3
+gr_max_lit=1.78
+
+pdf("output/compare_growth_rate.pdf")
+plot(seq_temp-273,val_growth_SV,ylim=c(0,2),pch=16,xlab="Temperature",ylab="Growth rate (d-1)")
+#abline(h=gr_min_Bissinger,lty=2)
+#abline(h=gr_max_Bissinger,lty=2)
+lines(seq_temp-273,val_growth_Biss,lty=2)
+abline(h=gr_min_lit,lty=1)
+abline(h=gr_max_lit,lty=1)
+legend("left",c("SV","Bissinger","Literature"),pch=c(16,NA,NA),lty=c(NA,2,1),bty="n")
+dev.off()
