a85abf0d6c7cb09ecee3172c5d3c84ef8c3a03de
Coralie Picoche <coralie.picoche@u-bordeaux.fr>
2020-04-16 22:12:08 +0200

    Corrected calibration files, interaction matrix for 1 and 2 first calibration

--- output/modelv1.1/main_functions.r
+++ output/modelv1.1/main_functions.r
@@ -1,7 +1,6 @@
 #################
-###CP 18/02/2020: Clean version of the main function 
-###CP 08/04/2020: Main function for the saturating interaction model
-###CP 16/04/20202: Added calibration on H_ij
+###CP 18/02/2020: Clean version of the main function
+###CP 16/04/2020: Version for main calibration
 #################
 
 rm(list=ls())
@@ -9,8 +8,9 @@ graphics.off()
 set.seed(40) #Warning: results and even feasibility can be highly seed-sensitive
 
 source("../../script/matrix_MAR_clean.r")
+source("../../script/infer_interaction_matrix_growth_rate.r")
 source("../../script/summary_statistics.r")
-source("compute_saturating_interactions.r")
+source("step_functions.r")
 
 ####Calibration
 value=c(0.9,1.1) #Parameter space
@@ -35,9 +35,7 @@ quad_prog=tab[tab[,1]=="quad_prog",2]
 n_iter=as.numeric(as.character(tab[tab[,1]=="n_iter",2]))
 temp_germin=as.numeric(as.character(tab[tab[,1]=="germin_threshold",2]))
 a_d=as.numeric(as.character(tab[tab[,1]=="daylength",2]))
-#threshold=as.numeric(as.character(tab[tab[,1]=="threshold",2]))
-O_y=as.numeric(as.character(tab[tab[,1]=="overyielding",2]))
-ratio_pos=as.numeric(as.character(tab[tab[,1]=="ratio_pos",2]))
+threshold=as.numeric(as.character(tab[tab[,1]=="threshold",2]))
 
 #Data to use (Auger)
 dataset=as.character(tab[tab[,1]=="dataset",2])
@@ -57,17 +55,13 @@ B_matrix=clean_matrix(cis,signif=F)
 rownames(B_matrix)=colnames(B_matrix)=name_spp
 nspp=length(name_spp)
 
-abundances_tab=read.table(paste("../../param/corres_hernandez_Auger.txt",sep=""),sep=";",header=T)
-#abundances_tab=read.table(paste("param/corres_hernandez_Auger.txt",sep=""),sep=";",header=T)
-dates=as.Date(abundances_tab$Date)
-abundances_tab=abundances_tab[year(dates)>=1996,name_spp]#Using data from 1996
-N_max=apply(abundances_tab,2,max,na.rm=T)
-N_mean=apply(abundances_tab,2,mean,na.rm=T)
+pop_table=read.table("../../param/abundances_Auger.txt",sep=",",dec=".",header=T)
+rownames(pop_table)=pop_table$sp
+pop_table=pop_table[name_spp,]
+x_obs=pop_table[name_spp,"Mean_abundances"]
+names(x_obs)=name_spp
 
-inter_mat=MAR2saturation(B_matrix,N_mean,N_max,O_y,ratio_pos)
-print("For now, interactions on the coast and in the ocean are the same")
-type_inter=list(inter_mat[[1]],inter_mat[[1]])
-list_H=list(inter_mat[[2]],inter_mat[[2]])
+inter_mat=MAR2BH(B_matrix,x_obs)
 
 #Sinking rates and T_opt
 tab=read.table(paste("species_specific_parameters.txt",sep=""),sep=";",dec=".",header=T)
@@ -82,8 +76,8 @@ B=tab$Val_b
 names(B)=name_spp
 r_mean=growth_rate_noMTE_Bissinger(288,T_opt,B,a_d)
 
-print("No quadratic programming available for saturating interactions, for now")
-write.table(cbind(inter_mat[[1]],r_mean),paste("matrix_A_before_quad.csv",sep=""),sep=";",row.names=F,dec=".")
+#write.table(cbind(inter_mat,r_mean),paste("matrix_A_before_quad.csv",sep=""),sep=";",row.names=F,dec=".")
+
 
 if(quad_prog==1){
 	tmp=quadprog(inter_mat,x_obs,r_mean,tol=10000,r_calibrate=F)
@@ -92,18 +86,16 @@ if(quad_prog==1){
 }else{
 	growth_rate=r_mean
 }
-write.table(cbind(inter_mat[[1]],growth_rate),paste("matrix_A_after_quad.csv",sep=""),sep=";",row.names=F,dec=".")
-
-#list_inter=list(inter_mat,k_coast2ocean*inter_mat)
+#write.table(cbind(inter_mat,growth_rate),paste("matrix_A_after_quad.csv",sep=""),sep=";",row.names=F,dec=".")
 
 #############Here, we begin the loop
-links_to_explore=which(inter_mat[[2]]!=0,arr.ind=T)
+links_to_explore=which(inter_mat!=0,arr.ind=T)
 nb_link=nrow(links_to_explore)
 
 name_links=c()
 #Write links, just for the sake of clarity
 for(l in 1:nb_link){
-        name_links=c(name_links,paste(links_to_explore[l,1],links_to_explore[l,2],sep="-"))
+	name_links=c(name_links,paste(links_to_explore[l,1],links_to_explore[l,2],sep="-"))
 }
 
 tab_simu=matrix(NA,nrow=nb_simu,ncol=nb_link)
@@ -116,29 +108,27 @@ colnames(tab_summary)=c("Abundance","Amplitude","Phenology","Diff")
 
 size_subset=floor(nb_link/(length(value)+1))
 
-pop_table=as.data.frame(cbind(names(N_mean),N_mean))
-pop_table[,2]=as.numeric(as.character(N_mean))
+
 tab_pheno=read.table("../../param/generalist_specialist_spp_added_amplitude_season.csv",sep=";",dec=".",header=T,row.names=1)
 
 t1=Sys.time()
 for(sim in 1:nb_simu){
-        print(sim)
-        tmp_inter=list_H[[1]]
-        set=1:nb_link
-        for(v in 1:length(value)){
-                inter_v=sample(set,size_subset,replace=F)
-                set_tmp=setdiff(set,inter_v)
-                for(i in 1:length(inter_v)){
-                        tmp_inter[links_to_explore[inter_v[i],1],links_to_explore[inter_v[i],2]]=list_H[[1]][links_to_explore[inter_v[i],1],links_to_explore[inter_v[i],2]]*value[v]
-                }
-                set=set_tmp
-                tab_simu[sim,inter_v]=value[v]
-        }
-        inter_no_change=setdiff(set,nb_link-size_subset*length(value))
-        tab_simu[sim,inter_no_change]=1.0
+	print(sim)
+	tmp_inter=inter_mat
+	set=1:nb_link
+	for(v in 1:length(value)){
+		inter_v=sample(set,size_subset,replace=F)
+		set_tmp=setdiff(set,inter_v)
+		for(i in 1:length(inter_v)){
+			tmp_inter[links_to_explore[inter_v[i],1],links_to_explore[inter_v[i],2]]=inter_mat[links_to_explore[inter_v[i],1],links_to_explore[inter_v[i],2]]*value[v]
+		}
+		set=set_tmp
+		tab_simu[sim,inter_v]=value[v]
+	}
+	inter_no_change=setdiff(set,nb_link-size_subset*length(value))
+	tab_simu[sim,inter_no_change]=1.0
 
-        list_H_tmp=list(tmp_inter,tmp_inter)
-
+	list_inter=list(tmp_inter,k_coast2ocean*tmp_inter)
 
 #Initialize
 N=array(NA,dim=c(n_iter,3,nspp),dimnames=list(NULL,c("coast","ocean","seed"),name_spp))
@@ -147,7 +137,7 @@ N[1,,]=rep(10^3,nspp*3)
 
 ##Run
 for(t in 1:(n_iter-1)){
-		var_tmp=step1(N[t,,],list_H_tmp,type_inter,temp_model[t],M,morta,a_d,T_opt,B)
+	        var_tmp=step1(N[t,,],list_inter,temp_model[t],M,morta,a_d,T_opt,B,threshold)
 		Ntmp=var_tmp[[1]]
 		effect_compet[t+1,,]=var_tmp[[2]]
         	N[t+1,,]=step2(Ntmp,S,Gamma*(temp_model[t]>=temp_germin),e)
@@ -158,7 +148,6 @@ final_summary=summary_statistics(pop_table,tab_pheno,tab_coast,nb_year)
 tab_summary[sim,1:3]=final_summary
 tab_summary[sim,4]=sum(final_summary)
 }
-
 print(Sys.time()-t1)
 
 #Write simulations
@@ -170,31 +159,31 @@ best=which(tab_summary[,4]==min(tab_summary[,4]))
 
 best_line_inter=tab_simu[best,]
 
-tmp_inter=list_H[[1]]
+tmp_inter=inter_mat
 for(l in 1:nrow(links_to_explore)){
-        tmp_inter[links_to_explore[l,1],links_to_explore[l,2]]=best_line_inter[l]*list_H[[1]][links_to_explore[l,1],links_to_explore[l,2]]
+	tmp_inter[links_to_explore[l,1],links_to_explore[l,2]]=best_line_inter[l]*inter_mat[links_to_explore[l,1],links_to_explore[l,2]]
 }
 
 ###Finally, we can run the model for the best simulation
-        list_H_tmp=list(tmp_inter,tmp_inter)
+        list_inter=list(tmp_inter,k_coast2ocean*tmp_inter)
 
 #Initialize
 N=array(NA,dim=c(n_iter,3,nspp),dimnames=list(NULL,c("coast","ocean","seed"),name_spp))
 effect_compet=array(NA,dim=c(n_iter,2,nspp),dimnames=list(NULL,c("coast","ocean"),name_spp))
 N[1,,]=rep(10^3,nspp*3)
 
-##Run
+##Run 
 for(t in 1:(n_iter-1)){
-                var_tmp=step1(N[t,,],list_H_tmp,type_inter,temp_model[t],M,morta,a_d,T_opt,B)
+                var_tmp=step1(N[t,,],list_inter,temp_model[t],M,morta,a_d,T_opt,B,threshold)
                 Ntmp=var_tmp[[1]]
                 effect_compet[t+1,,]=var_tmp[[2]]
                 N[t+1,,]=step2(Ntmp,S,Gamma*(temp_model[t]>=temp_germin),e)
 }
 
-write.table(list_H_tmp[[1]],"matrix_H_after_calibration.csv",sep=";",dec=".")
-write.table(type_inter,"faciliation_and_competition.csv",sep=";",dec=".")
+#Matrix
+write.table(tmp_inter,"matrix_A_after_calibration.csv",sep=";",dec=".")
 
-
+#Dynamics
 write.table(N[,1,],paste("out_coast.csv",sep=""),sep=";",dec=".")
 write.table(N[,2,],paste("out_ocean.csv",sep=""),sep=";",dec=".")
 write.table(N[,3,],paste("out_seed.csv",sep=""),sep=";",dec=".")
