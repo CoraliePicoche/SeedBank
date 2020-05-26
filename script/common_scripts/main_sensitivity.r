519a943de0d731b9fc3acc374a0a18bee9b08cb8
Coralie Picoche <coralie.picoche@u-bordeaux.fr>
2020-05-26 11:55:17 +0200

    Slightly modified the figure for model structure in exploratory

--- script/common_scripts/main_sensitivity.r
+++ script/common_scripts/main_sensitivity.r
@@ -3,16 +3,36 @@
 ##############
 
 graphics.off()
-if(1==0){
 
+if(1==0){
 rm(list=ls())
 
 source("step_functions.r")
 source("../summary_statistics.r")
 
-n_iter=1000
 nb_year=2
 cpt="ocean"
+nb_persistence=6*30 
+
+#Fixed parameters: golden parameter set
+tab=read.table("../../param/simu.csv",sep=";",dec=".",header=T)
+cyst_mortality=as.numeric(as.character(tab[tab[,1]=="cyst_mortality",2]))
+cyst_burial=as.numeric(as.character(tab[tab[,1]=="cyst_burial",2]))
+k_coast2ocean=as.numeric(as.character(tab[tab[,1]=="k_coast2ocean",2]))
+morta=as.numeric(as.character(tab[tab[,1]=="loss_rate",2]))
+e=as.numeric(as.character(tab[tab[,1]=="exchange",2]))
+germination=as.numeric(as.character(tab[tab[,1]=="germination",2]))
+resuspension=as.numeric(as.character(tab[tab[,1]=="resuspension",2]))
+S_max=as.numeric(as.character(tab[tab[,1]=="max_sinking",2]))
+quad_prog=tab[tab[,1]=="quad_prog",2]
+temp_germin=as.numeric(as.character(tab[tab[,1]=="germin_threshold",2]))
+a_d=as.numeric(as.character(tab[tab[,1]=="daylength",2]))
+threshold=as.numeric(as.character(tab[tab[,1]=="threshold",2]))
+
+n_iter=as.numeric(as.character(tab[tab[,1]=="n_iter",2]))
+id_persistence=seq(n_iter-nb_persistence+1,n_iter)
+id=seq(n_iter-365*nb_year+1,n_iter)
+
 
 #Data to use (Auger)
 a=as.matrix(read.table("../../param/reconstructed_temperature_Auger.txt", row.names=1,header=T,sep=";",dec="."))
@@ -26,27 +46,11 @@ pop_table=read.table("../../param/mean_interpolated_abundances_Auger.txt",sep=",
 rownames(pop_table)=pop_table$sp
 pop_table=pop_table[name_spp,]
 
-#Fixed parameters: golden parameter set
-tab=read.table("../../param/simu.csv",sep=";",dec=".",header=T)
-cyst_mortality=as.numeric(as.character(tab[tab[,1]=="cyst_mortality",2]))
-cyst_burial=as.numeric(as.character(tab[tab[,1]=="cyst_burial",2]))
-k_coast2ocean=as.numeric(as.character(tab[tab[,1]=="k_coast2ocean",2]))
-morta=as.numeric(as.character(tab[tab[,1]=="loss_rate",2]))
-e=as.numeric(as.character(tab[tab[,1]=="exchange",2]))
-germination=as.numeric(as.character(tab[tab[,1]=="germination",2]))
-resuspension=as.numeric(as.character(tab[tab[,1]=="resuspension",2]))
-S_max=as.numeric(as.character(tab[tab[,1]=="max_sinking",2]))
-quad_prog=tab[tab[,1]=="quad_prog",2]
-n_iter=as.numeric(as.character(tab[tab[,1]=="n_iter",2]))
-temp_germin=as.numeric(as.character(tab[tab[,1]=="germin_threshold",2]))
-a_d=as.numeric(as.character(tab[tab[,1]=="daylength",2]))
-threshold=as.numeric(as.character(tab[tab[,1]=="threshold",2]))
-
 #Sinking rates and T_opt
 tab=read.table(paste("../../param/species_specific_parameters.txt",sep=""),sep=";",dec=".",header=T)
 tab=tab[name_spp,]
 S=S_max*tab$S
-T_opt=tab$T_opt+273+5
+T_opt=tab$T_opt+273.15+5
 names(S)=name_spp
 names(T_opt)=name_spp
 B=tab$Val_b
@@ -94,23 +98,28 @@ colnames(list_simulation)=rownames(free_param)
 rownames(list_simulation)=1:nrow(list_simulation)
 
 ###############Store statistics
-tab_summary=array(NA,dim=c(nrow(list_simulation)+1,5,2),dimnames=list(1:(nrow(list_simulation)+1),c("Abundance","Amplitude","Phenology","Diff","Persistence"),c("modelI","modelII")))
+tab_summary=array(NA,dim=c(nrow(list_simulation)+1,6,2),dimnames=list(1:(nrow(list_simulation)+1),c("Abundance","Amplitude","Phenology","Diff","Persistence_coast","Persistence_ocean"),c("modelI","modelII")))
 
 for(m in 1:2){
 tab_coast=N_original_set[,cpt,,m]
 final_summary=summary_statistics(pop_table,tab_pheno,tab_coast,nb_year)
 tab_summary[nrow(list_simulation)+1,1:3,m]=c(sqrt(sum(final_summary[[1]]^2)/nspp),sqrt(sum(final_summary[[2]]^2)/nspp),sqrt(sum(final_summary[[3]]^2)/nspp))
 tab_summary[nrow(list_simulation)+1,4,m]=sum(tab_summary[nrow(list_simulation)+1,1:3,m])
-tab_summary[nrow(list_simulation)+1,5,m]=sum(tab_coast[nrow(tab_coast),]>0)
+
+tab_summary[nrow(list_simulation)+1,"Persistence_coast",m]=sum(apply(N_original_set[id_persistence,"coast",,m]==0,2,sum)<nb_persistence)
+tab_summary[nrow(list_simulation)+1,"Persistence_ocean",m]=sum(apply(N_original_set[id_persistence,"ocean",,m]==0,2,sum)<nb_persistence)
+
+
 }
 
 #Initialize
-N_sensitivity=array(NA,dim=c(n_iter,3,nspp,nrow(list_simulation),2),dimnames=list(NULL,c("coast","ocean","seed"),name_spp,1:nrow(list_simulation),c("modelI","modelII")))
-N_sensitivity[1,,,,]=10^3
+N_sensitivity=array(NA,dim=c(length(id),3,nspp,nrow(list_simulation),2),dimnames=list(NULL,c("coast","ocean","seed"),name_spp,1:nrow(list_simulation),c("modelI","modelII")))
 
 nb_simu=0
 for(param_to_move in rownames(free_param)){
+#for(param_to_move in rownames(free_param)[4]){
         for(value in free_param[param_to_move,c("Value_min","Value_max")]){
+#        for(value in free_param[param_to_move,c("Value_max")]){
                 nb_simu=nb_simu+1
                 #Keep other parameters
                 all_others=as.numeric(as.character(free_param[,"Value_used"]))
@@ -134,7 +143,8 @@ for(param_to_move in rownames(free_param)){
                 print(list_simulation[nb_simu,])
 
 ##Run
-N_simu=N_sensitivity[,,,nb_simu,]
+N_simu=array(NA,dim=c(n_iter,3,nspp,2),dimnames=list(NULL,c("coast","ocean","seed"),name_spp,c("modelI","modelII")))
+N_simu[1,,,]=10^3
 for(t in 1:(n_iter-1)){
 
 		var_tmp=step1_modelI(N_simu[t,,,1],list_inter_I,temp_model[t],M,morta,a_d,T_opt,B,threshold)
@@ -145,7 +155,7 @@ for(t in 1:(n_iter-1)){
                 Ntmp=var_tmp[[1]]
                 N_simu[t+1,,,2]=step2(Ntmp,S,Gamma*(temp_model[t]>=temp_germin),e)
 }
-                N_sensitivity[,,,nb_simu,]=N_simu[,,,]
+                N_sensitivity[,,,nb_simu,]=N_simu[id,,,]
 		for(m in 1:2){
                 tab_coast=N_simu[,cpt,,m]
                 if(sum(tab_coast==0)>0){ #One species has died
@@ -156,16 +166,19 @@ for(t in 1:(n_iter-1)){
                         tab_summary[nb_simu,1:3,m]=c(sqrt(sum(final_summary[[1]]^2)/nspp),sqrt(sum(final_summary[[2]]^2)/nspp),sqrt(sum(final_summary[[3]]^2)/nspp))
                         tab_summary[nb_simu,4,m]=sum(tab_summary[nb_simu,1:3,m])
                 }
-                tab_summary[nb_simu,5,m]=sum(tab_coast[nrow(tab_coast),]>0,na.rm=T)
-		}
-}
-}
-id=seq(n_iter-365,n_iter)
-analyses=rownames(list_simulation)
-}
+		tab_summary[nb_simu,"Persistence_coast",m]=sum(apply(N_simu[id_persistence,"coast",,m]==0,2,sum)<nb_persistence)
+		tab_summary[nb_simu,"Persistence_ocean",m]=sum(apply(N_simu[id_persistence,"ocean",,m]==0,2,sum)<nb_persistence)
 
+		}
+#		if(nb_simu==8){stop()}
+}
+}
+analyses=rownames(list_simulation)
 
 save(list = ls(all.names = TRUE), file = "main_sensitivity_simu.RData", envir = .GlobalEnv)
+} #end 1==0
+
+
 #Difference between abundance on the coast and on the ocean
 tmp_var=N_original_set[id,c("coast"),,]-N_original_set[id,c("ocean"),,]
 diff_tot_original=log10(abs(apply(tmp_var,c(2,3),mean)))
@@ -187,23 +200,23 @@ matrix_abundance_tot=array(NA,dim=c(nrow(free_param),2,ncol(amplitude_tot_origin
 for(param_to_move in rownames(free_param)){
         id_param=grep(paste("^",param_to_move,sep=""),analyses) #This will help issue an error if we have more than 2 values per parameter
         for(i in 1:length(id_param)){
-                mean_sens=log10(apply(N_sensitivity[id,cpt,,id_param[i],],c(2,3),mean))
+                mean_sens=log10(apply(N_sensitivity[,cpt,,id_param[i],],c(2,3),mean))
                 tmp_mean=100*(mean_tot_original-mean_sens)/mean_tot_original
                 matrix_mean[param_to_move,i,,]=tmp_mean
 
-                amp_sens=apply(log10(apply(N_sensitivity[id,cpt,,id_param[i],],c(2,3),range)),2,diff)
+                amp_sens=apply(log10(apply(N_sensitivity[,cpt,,id_param[i],],c(2,3),range)),2,diff)
                 tmp_amplitude=100*(amplitude_tot_original-amp_sens)/amplitude_tot_original
                 matrix_amplitude[param_to_move,i,,]=t(tmp_amplitude)
 
-                #Now, if the species disappeared, we set their stats to 0
-		for(m in 1:2){
-                matrix_mean[param_to_move,i,N_sensitivity[id[length(id)],cpt,,id_param[i],m]==0,m]=NA
-                matrix_amplitude[param_to_move,i,N_sensitivity[id[length(id)],cpt,,id_param[i],m]==0,m]=NA
-		}
-		tmp_var=N_sensitivity[id,c("coast"),,id_param[i],]-N_sensitivity[id,c("ocean"),,id_param[i],]
+                ##Now, if the species disappeared, we set their stats to 0
+		#for(m in 1:2){
+                #matrix_mean[param_to_move,i,N_sensitivity[id[length(id)],cpt,,id_param[i],m]==0,m]=NA
+                #matrix_amplitude[param_to_move,i,N_sensitivity[id[length(id)],cpt,,id_param[i],m]==0,m]=NA
+		#}
+		tmp_var=N_sensitivity[,c("coast"),,id_param[i],]-N_sensitivity[,c("ocean"),,id_param[i],]
 		matrix_diff[param_to_move,i,,]=log10(abs(apply(tmp_var,c(2,3),mean,na.rm=T)))
 
-		ab_tot=log10(apply(apply(N_sensitivity[id,c("ocean"),,id_param[i],],c(1,3),sum,na.rm=T),2,mean))
+		ab_tot=log10(apply(apply(N_sensitivity[,c("ocean"),,id_param[i],],c(1,3),sum,na.rm=T),2,mean))
 		matrix_abundance_tot[param_to_move,i,,]=100*(ab_tot_original-ab_tot)/ab_tot_original
 
         }
@@ -211,7 +224,7 @@ for(param_to_move in rownames(free_param)){
 
 pdf(paste("mean_abundance_amplitude_sensitivity.pdf",sep=""),width=17)
 par(mfrow=c(1,2))
-plot(0,0,t="n",xlim=c(0.75,nrow(free_param)+0.25),ylim=c(-7,4),xaxt="n",ylab="Average %change in log10(average abundance)",xlab="")
+plot(0,0,t="n",xlim=c(0.75,nrow(free_param)+0.25),ylim=c(-9,4),xaxt="n",ylab="Average %change in log10(average abundance)",xlab="")
 axis(1,labels=rownames(free_param),at=1:nrow(free_param))
 abline(h=0)
 mtext(c(all_others),1,line=3,at=1:nrow(free_param),cex=0.9)
@@ -229,16 +242,20 @@ for(param_to_move in rownames(free_param)){
                 per_change=mean(matrix_mean[param_to_move,i,,mod]*is.finite(matrix_mean[param_to_move,i,,mod]),na.rm=T)
 		if(mod==1){
 			colm="lightgrey"
-			idplus=0
+			idplus=idplus_text=0
 		}else{
 			colm="darkgrey"
 			idplus=0.08
+			idplus_text=0.
 		}
                 rect(l+0.75*seq_space[i]+idplus,min(c(0,per_change)),l+1.25*seq_space[i]+idplus,max(c(per_change,0)),col=colm)
+		print(analyses[i])
+		print(tab_summary[id_param[i],,mod])
                 if(tab_summary[id_param[i],4,mod]==0){
-			if(tab_summary[id_param[i],"Persistence",mod]<nspp){
+			if((tab_summary[id_param[i],"Persistence_coast",mod]<nspp)|(tab_summary[id_param[i],"Persistence_ocean",mod]<nspp)){
                         #text(l+seq_space[i]+idplus,per_change+(0.1*9)*sign(per_change),tab_summary[id_param[i],"Persistence",mod],col="red")
-                        text(l+seq_space[i]+idplus,3,tab_summary[id_param[i],"Persistence",mod],col="red")
+                        text(l+seq_space[i]+idplus,3,tab_summary[id_param[i],"Persistence_ocean",mod],col="red")
+#                        text(l+seq_space[i]+idplus_text,3-0.4,tab_summary[id_param[i],"Persistence_coast",mod],col="blue")
 			}else{
                         #points(l+seq_space[i]+idplus,per_change+(0.1*11)*sign(per_change),t="p",pch=16,col="red",cex=0.5)
                         points(l+seq_space[i]+idplus,3,t="p",pch=16,col="red",cex=0.5)
@@ -324,8 +341,8 @@ for(param_to_move in rownames(free_param)){
                 }
                 rect(l+0.75*seq_space[i]+idplus,min(c(0,per_change)),l+1.25*seq_space[i]+idplus,max(c(per_change,0)),col=colm)
                 if(tab_summary[id_param[i],4,mod]==0){
-			if(tab_summary[id_param[i],"Persistence",mod]<nspp){
-                        text(l+seq_space[i]+idplus,per_change+(0.1*9)*sign(per_change),tab_summary[id_param[i],"Persistence",mod],col="red")
+			if(tab_summary[id_param[i],"Persistence_ocean",mod]<nspp){
+                        text(l+seq_space[i]+idplus,per_change+(0.1*9)*sign(per_change),tab_summary[id_param[i],"Persistence_ocean",mod],col="red")
 			}else{
                         points(l+seq_space[i]+idplus,per_change+(0.1*9)*sign(per_change),t="p",pch=16,col="red",cex=0.5)
 			}
