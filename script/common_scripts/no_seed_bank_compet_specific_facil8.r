94e45f244e8a42f2b719499dd879fa51b6a899a0
Coralie Picoche <coralie.picoche@u-bordeaux.fr>
2020-07-15 18:34:31 +0200

    A few more words in Results + new analyses on results with higher and lower competition (is there a link between interactions from and to a species to explain its extinction

---
+++ script/common_scripts/no_seed_bank_compet_specific_facil8.r
@@ -0,0 +1,159 @@
+rm(list=ls())
+graphics.off()
+set.seed(42)
+
+source("step_functions.r")
+nb_year=2
+cpt="ocean"
+nb_persistence=6*30
+
+#Fixed parameters: golden parameter set
+tab=read.table("../../param/simu.csv",sep=";",dec=".",header=T)
+cyst_mortality=as.numeric(as.character(tab[tab[,1]=="cyst_mortality",2]))
+cyst_burial=as.numeric(as.character(tab[tab[,1]=="cyst_burial",2]))
+k_coast2ocean=as.numeric(as.character(tab[tab[,1]=="k_coast2ocean",2]))
+morta=as.numeric(as.character(tab[tab[,1]=="loss_rate",2]))
+e=as.numeric(as.character(tab[tab[,1]=="exchange",2]))
+germination=as.numeric(as.character(tab[tab[,1]=="germination",2]))
+resuspension=as.numeric(as.character(tab[tab[,1]=="resuspension",2]))
+S_max=as.numeric(as.character(tab[tab[,1]=="max_sinking",2]))
+quad_prog=tab[tab[,1]=="quad_prog",2]
+temp_germin=as.numeric(as.character(tab[tab[,1]=="germin_threshold",2]))
+a_d=as.numeric(as.character(tab[tab[,1]=="daylength",2]))
+threshold=as.numeric(as.character(tab[tab[,1]=="threshold",2]))
+
+n_iter=as.numeric(as.character(tab[tab[,1]=="n_iter",2]))
+id_persistence=seq(n_iter-nb_persistence+1,n_iter)
+id=seq(n_iter-365*nb_year+1,n_iter)
+
+
+#Data to use (Auger)
+a=as.matrix(read.table("../../param/reconstructed_temperature_Auger_with_corrected_phase_and_amplitude.txt", row.names=1,header=T,sep=";",dec="."))
+temp_model=a[1:n_iter]
+#Used for summary statistics
+tab_pheno=read.table("../../param/generalist_specialist_spp_added_amplitude_season.csv",sep=";",dec=".",header=T,row.names=1)
+name_spp=rownames(tab_pheno)
+nspp=length(name_spp)
+####Two options are possible for mean values
+pop_table=read.table("../../param/mean_interpolated_abundances_Auger.txt",sep=",",dec=".",header=T)
+rownames(pop_table)=pop_table$sp
+pop_table=pop_table[name_spp,]
+
+#Sinking rates and T_opt
+tab=read.table(paste("../../param/species_specific_parameters.txt",sep=""),sep=";",dec=".",header=T)
+tab=tab[name_spp,]
+S=S_max*tab$S
+T_opt=tab$T_opt+273.15+5
+names(S)=name_spp
+names(T_opt)=name_spp
+B=tab$Val_b
+names(B)=name_spp
+#Composite phenomena
+M=cyst_mortality+cyst_burial
+M_orig=M
+Gamma=resuspension*germination
+S=S_max*tab$S
+
+####Model I
+#Interaction
+tmp_inter_I=read.table("../../output/modelv1.1/interaction_matrix_after_calibration.csv",sep=";",dec=".")
+tmp_inter_I=as.matrix(tmp_inter_I)
+list_inter_I=list(tmp_inter_I,k_coast2ocean*tmp_inter_I) #Interaction list contains coastal and oceanic interactions
+
+####ModelII
+list_H_tmp=read.table("../../output/modelv2.1/interaction_matrix_after_calibration.csv",sep=";",dec=".")
+list_H=list(list_H_tmp,list_H_tmp)
+type_inter_tmp=read.table("../../output/modelv2.1/facilitation_and_competition.csv",sep=";",dec=".")
+type_inter_II=list(type_inter_tmp,k_coast2ocean*type_inter_tmp)
+
+######################Param changed when using seed_bank
+M_orig=M
+M=1
+fac_simu=8
+
+###Initialize
+N_array=array(NA,dim=c(length(id),3,nspp,length(fac_simu),2,2),dimnames=list(NULL,c("coast","ocean","seed"),name_spp,format(fac_simu,digits=3),c("compet","facil"),c("ModelI","ModelII")))
+N_orig=array(NA,dim=c(length(id),3,nspp,length(fac_simu),2,2),dimnames=list(NULL,c("coast","ocean","seed"),name_spp,format(fac_simu,digits=3),c("compet","facil"),c("ModelI","ModelII")))
+for(fac in 1:length(fac_simu)){
+        print(fac_simu[fac])
+        ####ModelI
+        #Compet
+        tmp_inter_compet=tmp_inter_I
+        tmp_inter_compet[which(tmp_inter_I>0,arr.ind=T)]=tmp_inter_I[which(tmp_inter_I>0,arr.ind=T)]*fac_simu[fac]
+        list_inter_compet=list(tmp_inter_compet,tmp_inter_compet)
+
+        #Facilitation
+        tmp_inter_facil=tmp_inter_I
+        tmp_inter_facil[which(tmp_inter_I<0,arr.ind=T)]=tmp_inter_I[which(tmp_inter_I<0,arr.ind=T)]*fac_simu[fac]
+        list_inter_facil=list(tmp_inter_facil,tmp_inter_facil)
+
+
+        ###ModelII
+        tmp_inter_compet=type_inter_tmp
+        tmp_inter_compet[which(type_inter_tmp>0,arr.ind=T)]=type_inter_tmp[which(type_inter_tmp>0,arr.ind=T)]*fac_simu[fac]
+        type_inter_compet=list(tmp_inter_compet,tmp_inter_compet)
+
+        #Facilitation
+        tmp_inter_facil=type_inter_tmp
+        tmp_inter_facil[which(type_inter_tmp<0,arr.ind=T)]=type_inter_tmp[which(type_inter_tmp<0,arr.ind=T)]*fac_simu[fac]
+        type_inter_facil=list(tmp_inter_facil,tmp_inter_facil)
+
+
+        N_array_simu=array(NA,dim=c(n_iter,3,nspp,2,2),dimnames=list(NULL,c("coast","ocean","seed"),name_spp,c("compet","facil"),c("ModelI","ModelII")))
+        N_array_simu[1,,,,]=10^3
+        N_orig_simu=array(NA,dim=c(n_iter,3,nspp,2,2),dimnames=list(NULL,c("coast","ocean","seed"),name_spp,c("compet","facil"),c("ModelI","ModelII")))
+        N_orig_simu[1,,,,]=10^3
+        for(t in 1:(n_iter-1)){
+                #ModelI         
+                #Without Seed Bank
+                #Compet
+                var_tmp=step1_modelI(N_array_simu[t,,,"compet","ModelI"],list_inter_compet,temp_model[t],M,morta,a_d,T_opt,B,threshold)
+                Ntmp=var_tmp[[1]]
+                N_array_simu[t+1,,,"compet","ModelI"]=step2(Ntmp,S,Gamma*(temp_model[t]>=temp_germin),e)
+
+                #Facil
+                var_tmp=step1_modelI(N_array_simu[t,,,"facil","ModelI"],list_inter_facil,temp_model[t],M,morta,a_d,T_opt,B,threshold)
+                Ntmp=var_tmp[[1]]
+                N_array_simu[t+1,,,"facil","ModelI"]=step2(Ntmp,S,Gamma*(temp_model[t]>=temp_germin),e)
+
+                #With Seed Bank
+                #Compet
+                var_tmp=step1_modelI(N_orig_simu[t,,,"compet","ModelI"],list_inter_compet,temp_model[t],M_orig,morta,a_d,T_opt,B,threshold)
+                Ntmp=var_tmp[[1]]
+                N_orig_simu[t+1,,,"compet","ModelI"]=step2(Ntmp,S,Gamma*(temp_model[t]>=temp_germin),e)
+
+                #Facil
+                var_tmp=step1_modelI(N_orig_simu[t,,,"facil","ModelI"],list_inter_facil,temp_model[t],M_orig,morta,a_d,T_opt,B,threshold)
+                Ntmp=var_tmp[[1]]
+                N_orig_simu[t+1,,,"facil","ModelI"]=step2(Ntmp,S,Gamma*(temp_model[t]>=temp_germin),e)
+
+                ##ModelII
+                #Without Seed Bank
+                #Compet
+                var_tmp=step1_modelII(N_array_simu[t,,,"compet","ModelII"],list_H,type_inter_compet,temp_model[t],M,morta,a_d,T_opt,B)
+                Ntmp=var_tmp[[1]]
+                N_array_simu[t+1,,,"compet","ModelII"]=step2(Ntmp,S,Gamma*(temp_model[t]>=temp_germin),e)
+
+                #Facil
+                var_tmp=step1_modelII(N_array_simu[t,,,"facil","ModelII"],list_H,type_inter_facil,temp_model[t],M,morta,a_d,T_opt,B)
+                Ntmp=var_tmp[[1]]
+                N_array_simu[t+1,,,"facil","ModelII"]=step2(Ntmp,S,Gamma*(temp_model[t]>=temp_germin),e)
+
+                #With Seed Bank
+                #Compet
+                var_tmp=step1_modelII(N_orig_simu[t,,,"compet","ModelII"],list_H,type_inter_compet,temp_model[t],M_orig,morta,a_d,T_opt,B)
+                Ntmp=var_tmp[[1]]
+                N_orig_simu[t+1,,,"compet","ModelII"]=step2(Ntmp,S,Gamma*(temp_model[t]>=temp_germin),e)
+
+                #Facil
+                var_tmp=step1_modelII(N_orig_simu[t,,,"facil","ModelII"],list_H,type_inter_facil,temp_model[t],M_orig,morta,a_d,T_opt,B)
+                Ntmp=var_tmp[[1]]
+                N_orig_simu[t+1,,,"facil","ModelII"]=step2(Ntmp,S,Gamma*(temp_model[t]>=temp_germin),e)
+
+
+
+        }
+        N_array[,,,fac,,]=N_array_simu[id,,,,]
+        N_orig[,,,fac,,]=N_orig_simu[id,,,,]
+}
+
