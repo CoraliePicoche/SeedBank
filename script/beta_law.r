0d15df7c3619ed04f75c336097cea1d487830467
Coralie Picoche <coralie.picoche@u-bordeaux.fr>
2020-11-09 18:02:27 +0100

    Update SI

--- script/beta_law.r
+++ script/beta_law.r
@@ -1,7 +1,7 @@
 rm(list=ls())
 graphics.off()
 
-val=c(9.5,5.1,9.6,8.1,6.0,8.3,15.5,7.8,19.7,9.6,6.6,3.7)
+val=c(9.5,5.1,9.6,8.1,6.0,8.3,15.5,7.8,19.7,9.6,6.6,3.7) #From Wiedmann et al. 2016, Table 2
 val_m=mean(val)
 
 dist_sinking=rbeta(10000,0.55,1.25)*30
